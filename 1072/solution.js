ec19f433daf194018ff9cf61d8cc072fe135f57e
Seungwoo321 <seungwoo321@gmail.com>
2023-05-11 13:38:52 +0900

    ac: solve 1072

---
+++ 1072/solution.js
@@ -0,0 +1,24 @@
+const filePath = require('path').join(__dirname, 'input');
+const [x, y] = require('fs')
+    .readFileSync(filePath)
+    .toString()
+    .trim()
+    .split(' ')
+    .map(BigInt);
+function solution (x, y) {
+    const z = (y * 100n) / x
+    let left = 1n;
+    let right = 1000000000n;
+    let min = -1n;
+    while (left <= right) {
+        const mid =(left + right) / 2n;
+        if (z === ((y + mid) * 100n) / ((x + mid))) {
+            left = mid + 1n;
+        } else {
+            right = mid - 1n;
+            min = mid;
+        }
+    }
+    return min.toString();
+}
+console.log(solution(x, y));
