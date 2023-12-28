ecd71191b3e0df975745f7c2e3ad5da8b539fd47
Seungwoo321 <seungwoo321@gmail.com>
2023-12-28 15:52:26 +0900

    feat: update 18290

--- silver/18290/solution.js
+++ silver/18290/solution.js
@@ -6,33 +6,38 @@ const [num, ...arr] = require('fs')
     .split('\n')
     .map(row => row.split(' ').map(Number));
 
-function solution (n, m, k, arr) {
-    const arr1 = []
-    const arr2 = []
-    for (let i = 0; i < n; i ++) {
-        for (let j = 0; j < m; j ++) {
-            let index = i * m + j;
-            if (index % 2 == 0) {
-                arr1.push(arr[i][j]);
-            } else {
-                arr2.push(arr[i][j])
+function solution (n, m, k, grid) {
+    const visited = Array.from({ length: n }, () => new Array(m).fill(0));
+    const distance = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+    
+    const result = [];
+    const dfs = ([y, x], depth, tmp) => {
+
+        if (depth === k) {
+            // return tmp.reduce(sum, 0);
+            result.push(tmp.slice())
+            return
+        }
+        
+        for (let i = y; i < n; i ++) {
+            for (let j = x; j < m; j ++) {
+                if (visited[i][j]) continue;
+                for (let [dy, dx] of distance) {
+                    const [ny, nx] = [dy + i, dx + j];
+                    if (ny === y || nx === x) {
+
+                    }
+                }
+                visited[i][j] = 1;
+                tmp.push(grid[i][j]);
+                dfs([i, j], depth + 1, tmp);
+                tmp.pop();
+                visited[i][j] = 0;
             }
         }
     }
-    const combination = (arr, r, depth, tmp) => {
-        if (depth === r) {
-            console.log(tmp)
-            return tmp.reduce((acc, cur) => acc + cur, 0);
-        }
-        const result = [];
-        for (let i = depth; i < arr.length; i ++) {
-            tmp[depth] = arr[i];
-            result.push(combination(arr, r, depth + 1, tmp))
-        }
-        return Math.max.apply(null, result);
-    }
-    
-    console.log(combination(arr1, k, 0, new Array(k).fill(0)))
+    dfs([0, 0], 0, []);
+    return result
 }
 
 console.log(solution(...num, arr))
